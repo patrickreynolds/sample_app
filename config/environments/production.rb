efdf3a26355dc007a2bba052c8c3647da9e3048b
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-02-04 23:17:40 -0800

    Update database gem versions.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ SampleApp::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.force_ssl = true
+  #config.force_ssl = true
 end

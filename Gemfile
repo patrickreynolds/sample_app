ebd134aed5675077979e5c25ab3d5bb179527f81
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-02-05 14:17:49 -0800

    Finish signin

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ ruby '2.1.0'
 gem 'rails', '4.0.2'
 gem 'bootstrap-sass', '3.1.0.1'
 gem 'pg', '0.17.1'
-#gem 'sqlite3', '1.3.8'
+gem 'sqlite3', '1.3.8'
 gem 'bcrypt-ruby', '3.1.2'
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 SampleApp::Application.routes.draw do
-  resources :users
   
+  # Root page
   root  'static_pages#home'
-  match '/signup',  to: 'users#new',            via: 'get'
+
+  # User pages
+  resources :users
+
+  # Static Pages
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+
+  # Authentication Routes
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signup',  to: 'users#new',          via: 'get'
+  match '/signin',  to: 'sessions#new',       via: 'get'
+  match '/signout', to: 'sessions#destroy',   via: 'get'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

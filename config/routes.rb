6d06a45b233ed4362f96fdb2f0a273b3286a675e
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-02-05 14:25:05 -0800

    Change http request type for sessions#destroy.

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ SampleApp::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   match '/signup',  to: 'users#new',          via: 'get'
   match '/signin',  to: 'sessions#new',       via: 'get'
-  match '/signout', to: 'sessions#destroy',   via: 'get'
+  match '/signout', to: 'sessions#destroy',   via: 'delete'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
